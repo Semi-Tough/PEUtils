a888bb7414b6acf7ef1d338c63bd460254a64257
Semi-Tough <1693416984@qq.com>
2022-09-26 17:45:41 +0800

    PETimer计时器抽象类

---
+++ PEUtils/PETimer/PETimer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PEUtils {
+    public abstract class PETimer {
+
+        protected int tid = 0;
+        public abstract int AddTask(uint delay, Action<int> taskCB, Action<int> cancleCB, int count = 1);
+        public abstract bool DeleteTask(int delay);
+        public abstract void Rest();
+        protected abstract int GenerateTid();
+
+    }
+}
